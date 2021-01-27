e39a78f08de9de7b23e0c9b9043bab050aa06b0c
Mike Rohloff <u34825@hs-harz.de>
2021-01-27 10:52:19 +0100

    Version1.0.1 Package Views hinzugefuegt

--- Staedteentfernung/src/de/softwaretechnik/view/ViewZiel.java
+++ Staedteentfernung/src/de/softwaretechnik/view/ViewZiel.java
@@ -9,29 +9,28 @@ public class ViewZiel extends JFrame implements Views {
 	
 	private int _width = 300;
 	private int _height = 100;
-	private JLabel lb_distance = new JLabel("Entfernung zum Ziel:", JLabel.CENTER);
+	private JLabel lb_distance = new JLabel( "Entfernung zum Ziel:", JLabel.CENTER );
 	private JTextField  tf_distance = new JTextField(6);
 	private JPanel _panel = new JPanel();
 	
 	public ViewZiel() {
-		setSize(_width, _height);
-		setTitle("Softwaretechnik View-Ziel Version 1.0.0");
+		setSize( _width, _height );
+		setTitle( "Softwaretechnik View-Ziel Version 1.0.0" );
 		setLayout( new java.awt.FlowLayout() );
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation( DISPOSE_ON_CLOSE );
 		
-    _panel.add(lb_distance);
-    _panel.add(tf_distance);
+    _panel.add( lb_distance );
+    _panel.add( tf_distance );
         
-    add(_panel);
+    add( _panel );
+	} // Konstruktor
 
-	}
-
-  public void setDistance(Node endPoint, ArrayList<Node> listNodes) {
-    for(Node element : listNodes) {
-	 	  if(element.getId() == endPoint.getId()) {
-		 	  tf_distance.setText(element.getDistance() + " km");
+  public void setDistance( Node endPoint, ArrayList<Node> listNodes ) {
+    for( Node element : listNodes ) {
+	 	  if( element.getId() == endPoint.getId() ) {
+		 	  tf_distance.setText( element.getDistance() + " km" );
 		  }
 	  }
-  }
+  } // setDistance
 
-}
+} // ViewZiel
